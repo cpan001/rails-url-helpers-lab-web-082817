5b072fa0ec6ad571f2ebf96739f22cdc667f67b6
cpan001 <github email address>
2017-09-21 19:46:16 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
+require 'pry'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    @student = set_student
+    status = @student.active
+    @student.active = !status
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
